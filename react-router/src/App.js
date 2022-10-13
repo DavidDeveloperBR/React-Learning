c15091f48f4207cf000db20fd60b8fff966bc776
David Rodrigues <david152012@hotmail.com>
2022-10-13 20:21:48 -0300

    React Router

---
+++ react-router/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+//config react router
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+
+//components
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>React Router</h1>
+        <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
